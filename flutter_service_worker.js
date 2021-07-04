43cf1af37ca2c2adf7bd81316867567fe608a3b0
Jeayoung Park <jeayoungpark1@gmail.com>
2021-07-04 18:40:21 -0400

    oh

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -17,9 +17,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "67d2c042c1d2dfbc996e347f27006d20",
-"/": "67d2c042c1d2dfbc996e347f27006d20",
-"main.dart.js": "bf36260a5ce3a293b6b66a7daef85706",
+"index.html": "c49ad14c91281deadec08ad936b1fd4a",
+"/": "c49ad14c91281deadec08ad936b1fd4a",
+"main.dart.js": "a4ba577bc30c910972bb7d66037e8c67",
 "manifest.json": "0e29d005fe5e6545a9df6c5653d6ce02",
 "version.json": "12e15db121c6fdd660fe06b2c9229d0c"
 };
